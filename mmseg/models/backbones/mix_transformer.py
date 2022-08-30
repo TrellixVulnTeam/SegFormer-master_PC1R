d4b9fdf5046ec64c6dc46443133f6ecf78cd44b6
Inch-Z <1666161794@qq.com>
2022-08-30 10:27:55 +0800

    temp

--- mmseg/models/backbones/mix_transformer.py
+++ mmseg/models/backbones/mix_transformer.py
@@ -16,18 +16,7 @@ from mmseg.models.builder import BACKBONES
 from mmseg.utils import get_root_logger
 from mmcv.runner import load_checkpoint
 import math
-class DWConv(nn.Module):
-    def __init__(self, dim=768):
-        super(DWConv, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
 
-    def forward(self, x, H, W):
-        B, N, C = x.shape
-        x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.dwconv(x)
-        x = x.flatten(2).transpose(1, 2)
-
-        return x
 
 rgb_to_ycbcr = torch.tensor([
     [ 0.299000,  0.587000,  0.114000],
@@ -216,6 +205,7 @@ class Mlp(nn.Module):
                 m.bias.data.zero_()
 
     def forward(self, x, H, W):
+        import ipdb;ipdb.set_trace()
         x = self.fc1(x)
         x = self.dwconv(x, H, W)
         x = self.act(x)
--- mmseg/models/backbones/mlp_mixer.py
+++ mmseg/models/backbones/mlp_mixer.py
@@ -1,6 +1,81 @@
+from torch import nn as nn
 import torch
-import torch.nn as nn
-from torch import functional as F
+from torch.nn import functional as F
+import einops
+class MLPBlock(nn.Module):
+    def __init__(self, mlp_dim:int, hidden_dim:int, dropout = 0.):
+        super(MLPBlock, self).__init__()
+        self.mlp_dim = mlp_dim
+        self.hidden_dim = hidden_dim
+        self.dropout = dropout
+        self.Linear1 = nn.Linear(mlp_dim, hidden_dim)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
+        self.Linear2 = nn.Linear(hidden_dim, mlp_dim)
+    def forward(self,x):
+        x = self.Linear1(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
+        x = self.Linear2(x)
+        x = self.dropout(x)
+        return x
+
+class Mixer_struc(nn.Module):
+    def __init__(self, patches: int , token_dim: int, dim: int,channel_dim: int,dropout = 0.):
+        super(Mixer_struc, self).__init__()
+        self.patches = patches
+        self.channel_dim = channel_dim
+        self.token_dim = token_dim
+        self.dropout = dropout
+
+        self.MLP_block_token = MLPBlock(patches,token_dim,self.dropout)
+        self.MLP_block_chan = MLPBlock(patches,channel_dim,self.dropout)
+        self.LayerNorm = nn.LayerNorm(dim)
+
+    def forward(self,x):
+        out = self.LayerNorm(x)
+        out = einops.rearrange(out, 'b n d -> b d n')
+        out = self.MLP_block_token(out)
+        out = einops.rearrange(out, 'b d n -> b n d')
+        out += x
+        out2 = self.LayerNorm(out)
+        out2 = self.MLP_block_chan(out2)
+        out2+=out
+        return out2
+
+
+class MLP_Mixer(nn.Module):
+    def __init__(self, image_size, patch_size, token_dim, channel_dim, num_classes, dim, num_blocks):
+        super(MLP_Mixer, self).__init__()
+        self.image_size = image_size//8
+        self.channel_dim = channel_dim
+        n_patches =(self.image_size//patch_size) **2
+        self.YCBCR = RGBToYCbCr()
+        self.DCT = DCTCompression()
+        self.patch_size_embbeder = nn.Conv2d(kernel_size=n_patches, stride=n_patches, in_channels=3, out_channels= dim)
+        self.blocks = nn.ModuleList([
+            Mixer_struc(patches=n_patches, token_dim=token_dim,channel_dim=self.channel_dim,dim=dim) for i in range(num_blocks)
+        ])
+        
+        self.Layernorm1 = nn.LayerNorm(dim)
+        self.classifier = nn.Linear(dim, num_classes)
+    def forward_features(self,x):
+        x = self.YCBCR(x)
+        x = self.DCT(x)
+        outs=[]
+        out = self.patch_size_embbeder(x)
+        out = einops.rearrange(out,"n c h w -> n (h w) c")
+        for block in self.blocks:
+            out = block(out)
+            outs.append(out)
+        return outs
+    def forward(self,x):
+        x = self.forward_features(x)
+        # out = self.Layernorm1(out)
+        # out = out.mean(dim = 1) # out（n_sample,dim）
+        # result = self.classifier(out)
+        # return result
+        return(x)
 
 rgb_to_ycbcr = torch.tensor([
     [ 0.299000,  0.587000,  0.114000],
@@ -85,7 +160,7 @@ def zigzag(n:int) -> torch.Tensor:
 class DCTCompression(nn.Module):
     def __init__(self, N=8, q=50):
         super().__init__()
-        self.RGB2YcBcR = RGBToYCbCr()
+
         self.N = N        
         
         # Create all N² frequency squares
@@ -159,3 +234,5 @@ class DCTCompression(nn.Module):
         # pdb.set_trace()
         return out
 
+model = MLP_Mixer(image_size= 256,patch_size= 16 , dim = 512 ,num_classes= 100,num_blocks=8,token_dim=256 , channel_dim=2048)
+print(model)
